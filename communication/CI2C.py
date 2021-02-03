94b5aed16c28a79c63b35cd418de7f863f555023
kukurihime <kukurihimeGit@gmail.com>
2021-02-03 23:28:14 +0900

    CI2C revised

--- communication/CI2C.py
+++ communication/CI2C.py
@@ -25,7 +25,9 @@ class CI2C:
     def detectAll(self):
         cmd = ['i2cdetect', '-y', str(self.i2cBusNum)]
         addr = subprocess.check_output(cmd)
-        addr = CStringUtil.combineChar( addr, ' ')
+        su = CStringUtil()
+        
+        addr = su.combineChar( addr, ' ')
         self.addressList = CStringUtil.splitMatrixBy( addr, ' ')
         
         
