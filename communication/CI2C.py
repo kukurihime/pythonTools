1bcc64812bb52969d6c610b4a633add08fdfc0be
kukurihime <kukurihimeGit@gmail.com>
2021-02-06 01:42:27 +0900

    CI2C revised

--- communication/CI2C.py
+++ communication/CI2C.py
@@ -35,7 +35,7 @@ class CI2C:
             self.addressList.append(tempAddr[1][1:])
             
         self.addressList = su.flatten(self.addressList)
-        self.addressList = [i for i in self.addressList if not i == '--' or '']
+        self.addressList = [i for i in self.addressList if not i == '--' or '\n']
         
 if __name__ == "__main__":
     i2c = CI2C()
