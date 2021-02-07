ab4dba50427b20d5281e6a69bf8a683b115f49f0
kukurihime <kukurihimeGit@gmail.com>
2021-02-08 00:50:54 +0900

    CI2C revised

--- communication/CI2C.py
+++ communication/CI2C.py
@@ -9,20 +9,32 @@ import smbus
 import subprocess
 import CStringUtil
 
-class CI2C:
+
+
+class CI2CAddressManager:
     def __init__(self):
         self.i2cBusNum = 1
         self.i2c = smbus.SMBus(self.i2cBusNum)
         self.mode = "master"
         self.addressList = []
+        self.previousAddrssList = []
         
-    def addAddress(self, addr):
-        self.addressList.append( addr )
-        
+    def addressList(self):
+        return self.addressList
+
     def addressNum(self):
         return len(self.addressList )
     
+    def addressAt(self, i):
+        if i >= self.addressNum():
+            return -1
+        elif i < 0:
+            return -1
+        else:
+            return self.addressList[i]
+    
     def detectAll(self):
+        self.previousAddrssList = self.addressList
         cmd = ['i2cdetect', '-y', str(self.i2cBusNum)]
         tempAddr = subprocess.check_output(cmd)
         su = CStringUtil.CStringUtil()
@@ -38,8 +50,12 @@ class CI2C:
         self.addressList = [i for i in self.addressList if not i == '--']
         self.addressList = [i for i in self.addressList if not i == '']
         
+    def divAddress(self):
+        return list(set(self.addressList) - set(self.previousAddrssList))
+    
+    
 if __name__ == "__main__":
-    i2c = CI2C()
-    i2c.detectAll()
-    print(i2c.addressList)
+    i2cam = CI2CAddressManager()
+    i2cam.detectAll()
+    print(i2cam.addressList)
     
