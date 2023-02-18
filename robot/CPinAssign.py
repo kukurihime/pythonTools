33e3d953664241d90f80baa82ac630805a79a390
kukurihime <kukurihimeGit@gmail.com>
2023-02-18 23:33:54 +0900

    revise robot

--- robot/CPinAssign.py
+++ robot/CPinAssign.py
@@ -6,47 +6,6 @@ Created on Sat Feb 11 21:35:38 2023
 @author: kukurihime
 """
 
-class CPin:
-    def __init__(self, pinNo, roll):
-        self.pinNo = pinNo
-        self.enable = False
-        self.rolls = [ 'input', 'output', 'pwm' ]
-        self.roll = ''
-        self.assignRoll(roll)
-        
-    def assignRoll(self, roll):
-        if roll == 'input':
-            self.roll = roll
-            self.enable = True
-        elif roll == 'output':
-            self.roll = roll
-            self.enable = True
-        elif roll == 'pwm':
-            self.roll = roll
-            self.enable = True
-        else:
-            self.roll = ''
-            self.enable = False
-    
-    def output(self):
-        pass
-    
-    def stop(self):
-        pass
-    
-class COutputPin(CPin):
-    def __init__(self, pinNo):
-        super().__init__(pinNo, 'output' )
-        self.H = 1
-        self.L = 0
-
-class CPwmPin(CPin):
-    def __init__(self, pinNo, freq = 500000):
-        super().__init__(pinNo, 'pwm' )
-        self.freq = freq #PWMFrequency
-        
-        
-        
 
 
 class CPinAssign:
@@ -54,10 +13,21 @@ class CPinAssign:
         self.outputPin =[]
         self.pwmPin = []
         
-    def addOutputPin(self, pinNo):
-        self.outputPin.append( pinNo )
+    def addOutputPin(self, pin):
+        self.outputPin.append( pin )
         
-    def addPwmPin(self, pinNo):
-        self.pwmPin.append( pinNo )
+    def addPwmPin(self, pin):
+        self.pwmPin.append( pin )
     
+if __name__ == "__main__":
+    import CRpiPin
+    
+    class test(CPinAssign):
+        def __init__(self):
+            super.__init__()
+            self.pwm1 = CRpiPin.CRpiPwmPin(12,500000)
+            self.addPwmPin(self.pwm1)
+            
+    t = test()
+    t.pwm1.on(0.8)
     
